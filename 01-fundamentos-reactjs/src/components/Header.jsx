1a1a1cddbdf9ff6dfb0c33ced4e10a6894a4f20f
Eduardo <edwardviana@gmail.com>
2024-10-04 17:01:27 -0300

    first commit

---
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css";
+
+import logo from "../assets/Ignite-logo.svg";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt="Logotipo" />
+      <strong>Iginite Feed</strong>
+    </header>
+  );
+}
